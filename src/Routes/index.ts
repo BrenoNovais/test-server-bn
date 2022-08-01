a14d38fd21e24922ac063fb7aea45e4c348c7816
BrenoNovais <breno.novais@hotmail.com>
2022-08-01 14:10:29 -0300

    home

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -2,13 +2,19 @@ import { Response, Router } from "express";
 
 export const Routes = Router()
 
+Routes.get("/", (req, res) => {
+    return res.json({
+        "message": "HOME MONTILA"
+    })
+})
+
 Routes.get("/home", (req, res) => {
     const Enviado = {
         "Enviado": "Ronaldo mandou",
         ...req.body
     }
 
-    
+
 
     return res.json(Enviado)
 })
