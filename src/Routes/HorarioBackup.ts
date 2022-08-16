441a172b2333c400717569dfb1312e21f1fc38d0
BrenoNovais <breno.novais@hotmail.com>
2022-08-16 10:03:04 -0300

    false ok

--- src/Routes/HorarioBackup.ts
+++ src/Routes/HorarioBackup.ts
@@ -5,6 +5,6 @@ export const HorarioBackupRoute = Router()
 
 HorarioBackupRoute.get('/horario-backup', HorarioBackupController.index1)
 
-HorarioBackupRoute.get('/horario-backup-false', HorarioBackupController.index1)
+HorarioBackupRoute.get('/horario-backup-false', HorarioBackupController.index2)
 
 export default HorarioBackupRoute
