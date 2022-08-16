17b177e93cb8267511603fb3f996c18ec670b05c
BrenoNovais <breno.novais@hotmail.com>
2022-08-16 10:00:26 -0300

    json

--- src/Controllers/HorarioBackup.ts
+++ src/Controllers/HorarioBackup.ts
@@ -2,12 +2,16 @@ import { Request, Response } from "express";
 
 export class HorarioBackupController {
 
-    static index1(req: Request, res: Response)  {
-        return res.json(true)
+    static index1(req: Request, res: Response) {
+        return res.json({
+            backup: true
+        })
     }
 
-    static index2(req: Request, res: Response)  {
-        return res.json(false)
+    static index2(req: Request, res: Response) {
+        return res.json({
+            backup: false
+        })
     }
 
 }
